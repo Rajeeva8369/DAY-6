a6f2230c00dd0233944b1975a4fd87db168dbe49
Rajeeva8369 <rajeeva.ravichandiran@epitech.digital>
2024-10-07 11:46:18 +0200

    ex_02.js

---
+++ ex_02/ex_02.js
@@ -0,0 +1,27 @@
+function addCar(agentString, car) {
+    let agent = JSON.parse(agentString);
+    agent.car = car;
+    return agent; 
+}
+
+const agentJSON = '{"age": 57, "code": "007", "firstName": "James", "lastName": "Bond"}';
+const carBrand = 'Aston Martin';
+
+const updatedAgent = addCar(agentJSON, carBrand);
+
+console.log(updatedAgent); 
+
+function displayUpdatedAgent(agent) {
+    const message = `Agent ${agent.firstName} ${agent.lastName} drives a ${agent.car}.`;
+    const p = document.querySelector('#container p');
+    
+    if (p) {
+        p.textContent = message;
+    } else {
+        console.error('Paragraph element not found');
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() { 
+    displayUpdatedAgent(updatedAgent); 
+});
